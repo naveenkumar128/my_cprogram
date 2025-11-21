8a5c11c10710128963c3a82c037f2f3542870c4c
naveenkumar128 <naveenkumarkanchinmath@gmail.com>
2025-11-21 15:01:39 +0530

    Add program to calculate average marks and result

---
+++ 7.SUBJECTS MARKS.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+   int m1,m2,m3;
+   float avg;
+   printf("Enter marks of three subjects:");
+   scanf("%d %d %d",&m1,&m2,&m3);
+   avg=(m1+m2+m3)/3.0;
+   printf("Avrage = %2f\n",avg);
+   if (m1,m2,m3>40)
+   printf("Result:passed\n");
+   else
+   printf("Result:failed\n");
+}
